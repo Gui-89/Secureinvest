407da2716c27f61a0095418b699560c4e8704cb8
Guilherme Alves da Silva <gadsv.terceirizado@trt10.jus.br>
2025-08-28 21:44:40 -0300

    Correção na página inicial

--- secureinvest.py
+++ secureinvest.py
@@ -20,7 +20,7 @@ try:
     REPORTLAB_AVAILABLE = True
 except ImportError:
     REPORTLAB_AVAILABLE = False
-    
+
 warnings.filterwarnings('ignore')
 
 # Configuração da página
@@ -2245,7 +2245,7 @@ def main():
     
     else:
         # Página inicial quando não há simulação
-        st.info("💡 Configure os parâmetros de investimento na barra lateral e clique em 'Simular Investimentos' para ver os resultados.")
+        st.info("💡 Configure os parâmetros de investimento na barra lateral, basta clicar nas setas no canto superior esquerdo (>>), pós preencher os campos desejados clique em 'Simular Investimentos' para ver os resultados.")
         
         col1, col2, col3, col4 = st.columns(4)
         
